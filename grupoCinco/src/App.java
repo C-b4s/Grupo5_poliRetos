3dcd43b68b6bb1042e48c2fd3c70ced8f5967f7f
Msagay <sagnaymicaela13@gmail.com>
2025-11-07 21:13:49 -0500

    avance

--- grupoCinco/src/App.java
+++ grupoCinco/src/App.java
@@ -144,5 +144,57 @@ public class App {
         // ...
     }
 }
+package src.serieNumericas;
 
+public class Principal {
+    
+    public static void main(String[] args) {
+        serieNumericas series = new serieNumericas();
+        
+        System.out.println("--- INICIO DEL PROGRAMA DE GENERACIÓN DE SERIES (S1 - S12) ---");
+        
+        // Ejecución de todos los métodos en orden secuencial (S1 a S12)
+        
+        // S1: Fibonacci (FOR)
+        series.g5_generarS1Fibonacci();
+        
+        // S2: Alternante (FOR)
+        series.g5_generarS2Alternante();
+        
+        // S3: Fracciones Impares (WHILE)
+        series.g5_generarS3FraccionesImpares();
+        
+        // S4: Fracciones Pares (WHILE)
+        series.g5_generarS4FraccionesPares();
+        
+        // S5: Primos (WHILE + FOR)
+        series.g5_generarS5Primos();
+        
+        // S6: Cuadrados (DO-WHILE)
+        series.g5_generarS6Cuadrados();
+        
+        // S7: Aritmética +3 (FOR)
+        series.g5_generarS7Aritmetica3();
+        
+        // S8: Aritmética +5 (WHILE)
+        series.g5_generarS8Aritmetica5();
+        
+        // S9: Potencias de 2 (DO-WHILE)
+        series.g5_generarS9PotenciasDeDos();
+        
+        // S10: Potencias de 3 (WHILE)
+        series.g5_generarS10PotenciasDeTres();
+        
+        // S11: Aritmética +4 (DO-WHILE)
+        series.g5_generarS11Aritmetica4();
+        
+        // S12: n*(n+1) (FOR)
+        series.g5_generarS12FactorialCompuesto();
+        
+        // Cierra el Scanner al finalizar el programa
+        series.closeScanner();
+        
+        System.out.println("--- FIN DEL PROGRAMA ---");
+    }
+}
         
